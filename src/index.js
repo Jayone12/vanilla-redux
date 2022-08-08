eaa441e43ccc9b35e2a8332e18206b0922a3a2e5
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-08-09 07:25:57 +0900

    feat: Vanilla JS Project 초기 셋팅

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let count = 0;
+
+number.innerText = count;
+
+const updateText = () => {
+  number.innerText = count;
+};
+
+const handleAdd = () => {
+  count++;
+  updateText();
+};
+const handleMinus = () => {
+  count--;
+  updateText();
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
