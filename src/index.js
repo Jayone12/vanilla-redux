191e366b24824c76ad08f69077f600981cec0039
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-08-09 09:25:02 +0900

    Vanilla JS를 통한 redux 예제 생성

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
+const ADD = "ADD";
+const MINUS = "MINUS";
 
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const reduser = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
 };
+
+const countStore = createStore(reduser);
+
+const onChange = () => (number.innerText = countStore.getState());
+
+countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  count++;
-  updateText();
+  countStore.dispatch({ type: ADD });
 };
 const handleMinus = () => {
-  count--;
-  updateText();
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
