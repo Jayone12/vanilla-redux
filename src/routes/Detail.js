9353cb7c1e0f372f309bc4b27b6eef5b41816152
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-08-10 13:36:22 +0900

    feat: Redux react 사용 예제 connect 버전

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,19 @@
-function Detail() {
-  return <div>Detail</div>;
+import { connect } from "react-redux";
+import { useParams } from "react-router";
+
+function Detail({ toDos }) {
+  const { id } = useParams();
+  const toDo = toDos.find((toDo) => toDo.id === parseInt(id));
+  return (
+    <>
+      <h1>{toDo?.text}</h1>
+      <small>Created At: {toDo?.id}</small>
+    </>
+  );
 }
-export default Detail;
+
+function mapStateToProps(state) {
+  return { toDos: state };
+}
+
+export default connect(mapStateToProps)(Detail);
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import { connect } from "react-redux";
+import ToDo from "../components/ToDo";
+import { actionCreators } from "../store";
 
-function Home() {
+function Home({ toDos, addToDo }) {
   const [text, setText] = useState("");
   const onChange = (event) => {
     setText(event.target.value);
@@ -8,6 +11,7 @@ function Home() {
   const onSubmit = (event) => {
     event.preventDefault();
     setText("");
+    addToDo(text);
   };
 
   return (
@@ -17,8 +21,24 @@ function Home() {
         <input type="text" value={text} onChange={onChange} />
         <button>add</button>
       </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
     </>
   );
 }
 
-export default Home;
+// redux state로 부터 home component에 props로 전달한다.
+function mapStateToProps(state) {
+  return { toDos: state };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
